8a645add8de8d9467f11a1bf62d7bcd7aa815d6f
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-24 15:10:05 -0700

    Create address view

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,19 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Dechon Ryan on 5/24/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
