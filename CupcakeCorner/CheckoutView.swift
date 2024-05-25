d323332e2d5a2df99399fb6d50e72baeda070096
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-24 18:49:00 -0700

    Add properties to hold address info

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Dechon Ryan on 5/24/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
